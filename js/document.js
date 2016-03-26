979848696a9439399f558c6f26281180295d9842
doudou <wb-liguoxian@alibaba-inc.com>
2016-03-26 17:15:38 +0800

    none

--- js/document.js
+++ js/document.js
@@ -89,15 +89,15 @@ var levelData = [
                 options: [
                     {
                         attrValue: '发烧',
-                        imgURL: './img/1.jpg'
+                        imgURL: './images/1.jpg'
                     },
                     {
                         attrValue: '流行感冒',
-                        imgURL: './img/1.jpg'
+                        imgURL: './images/1.jpg'
                     },
                     {
                         attrValue: '普通感冒',
-                        imgURL: './img/1.jpg'
+                        imgURL: './images/1.jpg'
                     }
                 ]
             },
@@ -111,15 +111,15 @@ var levelData = [
                         options: [
                             {
                                 attrValue: '发烧',
-                                imgURL: './img/1.jpg'
+                                imgURL: './images/1.jpg'
                             },
                             {
                                 attrValue: '流行感冒',
-                                imgURL: './img/1.jpg'
+                                imgURL: './images/1.jpg'
                             },
                             {
                                 attrValue: '普通感冒',
-                                imgURL: './img/1.jpg'
+                                imgURL: './images/1.jpg'
                             }
                         ]
                     },
@@ -129,15 +129,15 @@ var levelData = [
                         options: [
                             {
                                 attrValue: '发烧',
-                                imgURL: './img/1.jpg'
+                                imgURL: './images/1.jpg'
                             },
                             {
                                 attrValue: '流行感冒',
-                                imgURL: './img/1.jpg'
+                                imgURL: './images/1.jpg'
                             },
                             {
                                 attrValue: '普通感冒',
-                                imgURL: './img/1.jpg'
+                                imgURL: './images/1.jpg'
                             }
                         ]
                     }
@@ -165,7 +165,7 @@ levelData = JSON.stringify(levelData);
 
 var birdsData = [
     {
-        imgURL: './img/1.jpg', // 图片链接
+        imgURL: './images/1.jpg', // 图片链接
         chineseName: 'chineseName1', // 中文名
         latinName: 'latinName', // 拉丁名
         alias: 'alias', // 别名
@@ -175,7 +175,7 @@ var birdsData = [
         description: 'description' // 描述
     },
     {
-        imgURL: './img/1.jpg', // 图片链接
+        imgURL: './images/1.jpg', // 图片链接
         chineseName: 'chineseName2', // 中文名
         latinName: 'latinName', // 拉丁名
         alias: 'alias', // 别名
@@ -185,7 +185,7 @@ var birdsData = [
         description: 'description' // 描述
     },
     {
-        imgURL: './img/1.jpg', // 图片链接
+        imgURL: './images/1.jpg', // 图片链接
         chineseName: 'chineseName3', // 中文名
         latinName: 'latinName', // 拉丁名
         alias: 'alias', // 别名
@@ -195,7 +195,7 @@ var birdsData = [
         description: 'description' // 描述
     },
     {
-        imgURL: './img/1.jpg', // 图片链接
+        imgURL: './images/1.jpg', // 图片链接
         chineseName: 'chineseName4', // 中文名
         latinName: 'latinName', // 拉丁名
         alias: 'alias', // 别名
@@ -208,73 +208,7 @@ var birdsData = [
 
 birdsData = JSON.stringify(birdsData);
 
-// <!-- 具体项 -->
-//                 <div class="col-md-4">
-//                     <div class="thumbnail">
-//                         <img src="./img/1.jpg" alt="bird">
-//                         <div class="caption">
-//                             <h3>这个是什么鸟</h3>
-//                             <p>这个是某某鸟blablabla...</p>
-//                             <p>
-//                                 <a href="#" class="btn btn-primary" role="button" data-toggle="modal" data-target=".bs-example-modal-lg">图片</a>
-//                                 <a href="#" class="btn btn-default" role="button">详情</a>
-//                             </p>
-//                         </div>
-//                     </div>
-//                     <!-- 详情模态框 -->
-//                     <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
-//                         <div class="modal-dialog modal-lg">
-//                             <div class="modal-content">
-//                                 <!-- 图集 -->
-//                                 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-//                                     <!-- Indicators -->
-//                                     <ol class="carousel-indicators">
-//                                         <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-//                                         <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-//                                         <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-//                                     </ol>
+birdDetailData = {
+    
+}
 
-//                                     <!-- Wrapper for slides -->
-//                                     <div class="carousel-inner" role="listbox">
-//                                         <div class="item active">
-//                                             <img src="./img/1.jpg" alt="bird">
-//                                             <div class="carousel-caption">这种鸟是blablabla...</div>
-//                                         </div>
-//                                         <div class="item">
-//                                             <img src="./img/1.jpg" alt="bird">
-//                                             <div class="carousel-caption">这种鸟是blablabla...</div>
-//                                         </div>
-//                                         <div class="item">
-//                                             <img src="./img/1.jpg" alt="bird">
-//                                             <div class="carousel-caption">这种鸟是blablabla...</div>
-//                                         </div>
-//                                     </div>
-
-//                                     <!-- Controls -->
-//                                     <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-//                                         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-//                                         <span class="sr-only">Previous</span>
-//                                     </a>
-//                                     <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-//                                         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-//                                         <span class="sr-only">Next</span>
-//                                     </a>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 <!-- 具体项 -->
-//                 <div class="col-md-4">
-//                     <div class="thumbnail">
-//                         <img src="./img/1.jpg" alt="bird">
-//                         <div class="caption">
-//                             <h3>这个是什么鸟</h3>
-//                             <p>这个是某某鸟blablabla...</p>
-//                             <p>
-//                                 <a href="#" class="btn btn-primary" role="button">图片</a>
-//                                 <a href="#" class="btn btn-default" role="button">详情</a>
-//                             </p>
-//                         </div>
-//                     </div>
-//                 </div>
